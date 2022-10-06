66632973a71fcf14abd96a75bc9218fe39fc5aa2
Drew Milbrath <milbrathdrew@gmail.com>
2022-10-05 22:35:43 -0600

    Prod Release Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  //property is empty on first instance of this class
+  password = '';
+  
+  
+  onChangeLength(event: Event){
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  //when the method is called
+  //update the value to the opposite of current value
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  //when onButtonClick method is called, update value of password
+  onButtonClick(){
+    //list all possible values
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*()_+'
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters; 
+    }
+
+    if (this.includeNumbers){
+      validChars += numbers;
+    }
+
+    if (this.includeNumbers){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    //this log will display the value of includeLetters
+    console.log(this.includeLetters);
+    console.log(this.includeNumbers);
+    console.log(this.includeSymbols);
+
+    console.log(`
+    About to generate a password with the following:
+    Includes Letters: ${this.includeLetters}
+    Includes Numbers: ${this.includeNumbers}
+    Includes Symbols: ${this.includeSymbols}
+    `)
+    this.password = generatedPassword;
+  }
+
+
+
 }
